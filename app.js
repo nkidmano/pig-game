2d34098b4ce8c3e8da01b3ac93aaf1a765f3e8dd
nkidmano <nobudy55@gmail.com>
2018-06-12 00:30:37 +0700

    finish basic pig-game

--- app.js
+++ app.js
@@ -9,56 +9,77 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 const winCondition = 100;
 
-scores = [0, 0];
-roundScore = 0; 
-activePlayer = 0;
-
-document.querySelector(`.dice`).style.display = `none`;
-
-document.getElementById(`score-0`).textContent = `0`;
-document.getElementById(`score-1`).textContent = `0`;
-document.getElementById(`current-0`).textContent = `0`;
-document.getElementById(`current-1`).textContent = `0`;
+init();
 
 document.querySelector(`.btn-roll`).addEventListener(`click`, function() {
-  const dice = Math.floor(Math.random() * 6) + 1;
-  const diceDOM = document.querySelector(`.dice`);
-  
-  diceDOM.style.display = `block`;
-  diceDOM.src = `dice-${dice}.png`;
-  
-  if (dice !== 1) {
-    // Add score
-    roundScore += dice;
-    document.getElementById(`current-${activePlayer}`).textContent = roundScore;
-  } else {
-    // Next player
-    nextPlayer();
+  if (gamePlaying) {
+    // Generate random number 
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    // Display result
+    const diceDOM = document.querySelector(`.dice`);
+    diceDOM.style.display = `block`;
+    diceDOM.src = `dice-${dice}.png`;
+    
+    if (dice !== 1) {
+      // Add score
+      roundScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+    } else {
+      // Next player
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector(`.btn-hold`).addEventListener(`click`, function() {
-  // Add CURRENT score to GLOBAL score
-  scores[activePlayer] += roundScore;
-  
-  // Update UI
-  document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
-  document.getElementById(`current-${activePlayer}`).textContent = 0;
+  if (gamePlaying) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+    
+    // Update UI
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
 
-  // Check if the player won the game
-  if (scores[activePlayer] >= 10) {
-    document.querySelector(`#name-${activePlayer}`).textContent = `Winner!!!`;
-    document.querySelector(`.player-${activePlayer}-panel`).classList.add(`winner`); 
-    document.querySelector(`.player-${activePlayer}-panel`).classList.remove(`active`); 
-    document.querySelector(`.dice`).style.display = `none`;
-  } else {
-    // Next player
-    nextPlayer();
+    // Check if the player won the game
+    if (scores[activePlayer] >= 10) {
+      // Stop the game
+      gamePlaying = false
+      // Update UI
+      document.querySelector(`#name-${activePlayer}`).textContent = `Winner!!!`;
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add(`winner`); 
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove(`active`); 
+      document.querySelector(`.dice`).style.display = `none`;
+    } else {
+      // Next player
+      nextPlayer();
+    }
   }
 });
+
+document.querySelector(`.btn-new`).addEventListener(`click`, init);
+
+function init() {
+  gamePlaying = true;
+  scores = [0, 0];
+  roundScore = 0; 
+  activePlayer = 0;
+  
+  document.querySelector(`.player-0-panel`).classList.add(`active`);
+  document.querySelector(`.dice`).style.display = `none`;
+  
+  document.getElementById(`score-0`).textContent = `0`;
+  document.getElementById(`score-1`).textContent = `0`;
+  document.getElementById(`current-0`).textContent = `0`;
+  document.getElementById(`current-1`).textContent = `0`;
+  document.getElementById(`name-0`).textContent = `PLAYER 1`;
+  document.getElementById(`name-1`).textContent = `PLAYER 2`;
+  document.querySelector(`.player-0-panel`).classList.remove(`winner`);
+  document.querySelector(`.player-1-panel`).classList.remove(`winner`);
+}
 
 function nextPlayer() {
   activePlayer = (activePlayer === 0) ? 1 : 0;
